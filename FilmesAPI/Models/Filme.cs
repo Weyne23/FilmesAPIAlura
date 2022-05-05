13f86b0eadb9a6e8f68d2888b15cbbc62328ccb7
Weyne <augustoweyne12@gmail.com>
2022-05-05 00:25:07 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo titulo é obrigratorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigratorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O campo genero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 400, ErrorMessage = "A duração minima é 1 e maxima 600")]
+        public int Duracao { get; set; }
+    }
+}
